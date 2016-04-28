Fixed indexing error in backprop.

autoencoder.py
@@ -79,20 +79,23 @@ class Autoencoder(object):
     partial_deriv_w = np.array([np.zeros(w_.shape) for w_ in self.weights])
     # Go through each example in the batch and sum up the partial
     # derivatives from each with respect to b and w.
-    for x in batch:
+    for x in batch: 
       single_partial_deriv_b, single_partial_deriv_w = self.backprop(x, x)
+      assert(np.array(single_partial_deriv_w).shape == np.array(partial_deriv_w).shape)
       partial_deriv_b = partial_deriv_b + np.array(single_partial_deriv_b)
-      partial_deriv_w = partial_deriv_w + np.array(single_partial_deriv_w)
+      #print partial_deriv_w
+      #print single_partial_deriv_w
+      partial_deriv_w = partial_deriv_w + single_partial_deriv_w
       #partial_deriv_b = [pdb+spdb for pdb, spdb in zip(partial_deriv_b, single_partial_deriv_b)]
       #partial_deriv_w = [pdw+spdw for pdw, spdw in zip(partial_deriv_w, single_partial_deriv_w)]
     # Update the weights and biases using the calculated partial derivatives.
     self.biases = np.array(self.biases) - (learning_coef/len(batch))*partial_deriv_b
     self.weights = np.array(self.weights) - (learning_coef/len(batch))*partial_deriv_w
-    #self.biases = [b-(learning_coef/len(batch))*deriv_b
-    #               for b, deriv_b in zip(self.biases, partial_deriv_b)]
-    #self.weights = [w-(learning_coef/len(batch))*deriv_w
-    #                for w, deriv_w in zip(self.weights, partial_deriv_w)]
-
+    '''self.biases = [b-(learning_coef/len(batch))*deriv_b
+                   for b, deriv_b in zip(self.biases, partial_deriv_b)]
+    self.weights = [w-(learning_coef/len(batch))*deriv_w
+                    for w, deriv_w in zip(self.weights, partial_deriv_w)]
+    '''
   '''
   Return a tuple (partial_deriv_b, partial_deriv_w) representing the gradient
   for the cost function. The partial derivatives are of the cost function with
@@ -121,17 +124,16 @@ class Autoencoder(object):
 
     # Go through each layer backwards and propagate the error.
     L = self.num_layers-1
-    print(np.array(activations).shape)
     errors = (activations[L] - y)*sigmoid_deriv(zs[L-1])
     # There's one less set of weights/biases than the number of layers
     # because the input layer doesn't have weights or biases. 
     partial_deriv_b[L-1] = errors
-    partial_deriv_w[L-1] = np.dot(errors, activations[L-2].transpose())
-    for l in xrange(L-2, 1, -1):
+    partial_deriv_w[L-1] = np.dot(errors, np.transpose(activations[L-1]))
+    for l in xrange(L-2, 0, -1):
       errors = np.dot(np.transpose(self.weights[l+1]), errors) \
                * sigmoid_deriv(zs[l])
       partial_deriv_b[l] = errors
-      partial_deriv_w[l] = np.dot(activations[l-1], errors)
+      partial_deriv_w[l] = np.dot(errors, np.transpose(activations[l]))
     
     return (partial_deriv_b, partial_deriv_w)
 